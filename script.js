ad43820e9998de6a5dc8f124a8c2abaf551c2f82
[Usamashah777] <[usamanedo@gmail.com]>
2025-05-24 13:17:36 +0500

    first assignenmt

---
+++ script.js
@@ -0,0 +1,24 @@
+// Firstname
+
+var firstName=document.getElementsByClassName('fn')
+
+
+// Last name 
+var LastName=document.getElementsByClassName('fl')
+
+var sumOffNamelName
+
+firstName=LastName
+
+document.write(LastName)
+
+// button 
+var bSub=document.getElementsByClassName('sub')
+
+// after Submition SHowing Value
+var sName=document.getElementsByClassName('showing-value')
+
+
+
+
+
