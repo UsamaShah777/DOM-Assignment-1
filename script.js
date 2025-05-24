f7d34d1e0c1ed84019eff0c4c1eee83bc9166c28
[Usamashah777] <[usamanedo@gmail.com]>
2025-05-24 15:32:53 +0500

    with colr

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// function showValues() {
-//   var firstName = document.getElementById("firstname").value;
-//   var lastName = document.getElementById("lastname").value;
 
   function showValues(){
     var firstName=document.getElementById('firstname').value
@@ -10,16 +7,16 @@
   var outputDiv = document.getElementById("output");
   outputDiv.innerHTML = "First Name: " + firstName + "<br>Last Name: " + lastName;
 
-  // Random color generate karna
+
   var randomColor = getRandomColor();
 
-  // Background color set karna
+
   document.body.style.backgroundColor = randomColor;
 }
 
-// Random HEX color banane wala function
+
 function getRandomColor() {
-  // Random HEX color e.g., #a3e12f
+
   var letters = "0123456789ABCDEF";
   var color = "#";
   for (var i = 0; i < 6; i++) {
